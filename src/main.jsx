cead409dcd710b27f3df55734be1d6b6519e40b5
Valerie Freeman <valerah7@gmail.com>
2024-03-22 15:18:17 -0500

    setup radix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { App } from "./App.jsx"
 import "./index.css"
+import "@radix-ui/themes/styles.css"
+import { Theme } from "@radix-ui/themes"
 
 const container = document.getElementById("root")
 const root = ReactDOM.createRoot(container)
-root.render(<App />)
+root.render(
+  <Theme>
+    <App />
+  </Theme>
+)
